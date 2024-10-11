e912ab7b9ce1009ec6320bd6c1b22b31485bb78b
Akash Vinayak Bhandari <143438845+Akashbh011@users.noreply.github.com>
2024-10-11 23:42:10 +0530

    dynamic routing !

--- src/components/mapComponent/mapComponent.jsx
+++ src/components/mapComponent/mapComponent.jsx
@@ -7,7 +7,6 @@ import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
-
 // Set default icon for Leaflet markers
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -16,15 +15,16 @@ L.Icon.Default.mergeOptions({
   shadowUrl: markerShadow,
 });
 
-
 const MapComponent = () => {
   const [position, setPosition] = useState(null);
+  const specificCoordinates = [18.521279, 73.855104]; // The specific coordinates for the marker
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
         setPosition([latitude, longitude]);
+        console.log(`User's Location - Longitude: ${longitude}, Latitude: ${latitude}`);
       },
       (error) => {
         console.error(error);
@@ -35,13 +35,14 @@ const MapComponent = () => {
   return (
     <div className="map-container">
       {position ? (
-        <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
+        <MapContainer center={specificCoordinates} zoom={10} style={{ height: '100%', width: '100%' }}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
-            <Popup>You are here!</Popup>
+          {/* Marker at the specific coordinates */}
+          <Marker position={specificCoordinates}>
+            <Popup>Marker at 18.521279, 73.855104</Popup>
           </Marker>
         </MapContainer>
       ) : (
@@ -52,3 +53,60 @@ const MapComponent = () => {
 };
 
 export default MapComponent;
+
+
+// import React, { useEffect, useState } from 'react';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css';
+// import L from 'leaflet';
+// import './mapComponent.css';
+// import markerIcon from 'leaflet/dist/images/marker-icon.png';
+// import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+// import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+
+// // Set default icon for Leaflet markers
+// delete L.Icon.Default.prototype._getIconUrl;
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: markerIcon2x,
+//   iconUrl: markerIcon,
+//   shadowUrl: markerShadow,
+// });
+
+
+// const MapComponent = () => {
+//   const [position, setPosition] = useState(null);
+
+//   useEffect(() => {
+//     navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         const { latitude, longitude } = position.coords;
+//         setPosition([latitude, longitude]);
+//         console.log(longitude+"  "+latitude);
+//       },
+//       (error) => {
+//         console.error(error);
+//       }
+//     );
+//   }, []);
+
+//   return (
+//     <div className="map-container">
+//       {position ? (
+//         <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
+//           <TileLayer
+//             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//           />
+//           <Marker position={position}>
+//             <Popup>You are here!</Popup>
+//           </Marker>
+//         </MapContainer>
+//       ) : (
+//         <p>Loading your location...</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MapComponent;
