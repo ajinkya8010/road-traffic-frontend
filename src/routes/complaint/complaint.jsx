d8c41aaa8c8ab363d211517c72becd849390e085
ajinkya8010 <ajinkya8975@gmail.com>
2025-01-03 15:33:55 +0530

    -

--- src/routes/complaint/complaint.jsx
+++ src/routes/complaint/complaint.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Complaint.css';
+import './complaint.css';
 import apiRequest from '../../lib/apiRequest';
 
 const Complaint = () => {
